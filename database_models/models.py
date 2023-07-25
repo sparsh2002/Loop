f0da3e3638e549d3867b4be449c20c307987ab87
sparsh2002 <ui20ec62@iiitsurat.ac.in>
2023-07-25 08:50:58 +0530

    models defined

---
+++ database_models/models.py
@@ -0,0 +1,24 @@
+import pymongo
+from database.conn import client
+
+db = client['loop']
+
+class Store:
+    def __init__(self, store_id, status, timestamp_utc):
+        self.store_id = store_id
+        self.status = status
+        self.timestamp_utc = timestamp_utc
+
+
+class Menu:
+    def __init__(self , store_id , day , start_time_local , end_time_local):
+        self.store_id = store_id
+        self.day = day
+        self.start_time_local = start_time_local
+        self.end_time_local = end_time_local
+
+
+class Bq:
+    def __init__(self , store_id , timezone_str):
+        self.store_id = store_id
+        self.timezone_str = timezone_str
